4617f4482170ee8f2ac9c6cf51117661179e7e44
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-06 14:54:21 +0900

    Create upperBound.swift

---
+++ upperBound.swift
@@ -0,0 +1,18 @@
+let nums: [Int] = [ 1, 2, 3, 3, 3, 4, 5, 6 ,7 ,8 ]
+
+func upperBound(target: Int) -> Int { // index return
+    var start: Int = 0
+    var end: Int = nums.count
+
+    while start < end {
+        let mid: Int = (start + end) / 2
+
+        if nums[mid] <= target {
+            start = mid + 1
+        } else {
+            end = mid
+        }
+    }
+
+    return end
+}
